c0caff98da3360a570f087d0b3025ce9a980f5dc
ninja-ninja-arch <rajustha8027414@gmail.com>
2025-03-17 10:50:19 -0700

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -6,9 +6,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/droidx_violet.mk
+    $(LOCAL_DIR)/banana_violet.mk
 
 COMMON_LUNCH_CHOICES := \
-    droidx_violet-ap2a-eng \
-    droidx_violet-ap2a-userdebug \
-    droidx_violet-ap2a-user
+    banana_violet-ap2a-eng \
+    banana_violet-ap2a-userdebug \
+    banana_violet-ap2a-user
