8810df9527372f7760feaaf77b04b313c1b30d81
ninja-ninja-arch <rajustha8027414@gmail.com>
2025-03-22 12:31:40 -0700

    Update and rename lineage_violet.mk to genesis_violet.mk

--- genesis_violet.mk
+++ genesis_violet.mk
@@ -8,10 +8,11 @@
 # Inherit from violet device
 $(call inherit-product, device/xiaomi/violet/device.mk)
 
-# Inherit common MistOs configurations
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
-
-$(call inherit-product, vendor/lineage/config/BoardConfigReservedSize.mk)
+# Inherit some common Genesis stuff.
+$(call inherit-product, vendor/genesis/config/common_full_phone.mk)
+GENESIS_MAINTAINER := NINJA
+GENESIS_OFFICIAL := false
+$(call inherit-product, vendor/genesis/config/BoardConfigReservedSize.mk)
 
 # MiuiCamera
 $(call inherit-product, vendor/MiuiCamera/config.mk)
@@ -38,7 +39,7 @@ TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_INCLUDE_PIXEL_CHARGER := true
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := lineage_violet
+PRODUCT_NAME := genesis_violet
 PRODUCT_DEVICE := violet
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi Note 7 Pro
