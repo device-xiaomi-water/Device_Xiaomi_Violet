3585c8b3adc30b80cfb64083d34ebef791d9165c
ninja-ninja-arch <rajustha8027414@gmail.com>
2025-03-22 12:34:48 -0700

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,14 +1,14 @@
 #
-# Copyright (C) 2018-2020 The LineageOS Project
+# Copyright (C) 2018-2020 The genesisOS Project
 # Copyright (C) 2020 The PixelExperience Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_violet.mk
+    $(LOCAL_DIR)/genesis_violet.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_violet-ap1a-eng \
-    lineage_violet-ap1a-userdebug \
-    lineage_violet-ap1a-user
+    genesis_violet-ap1a-eng \
+    genesis_violet-ap1a-userdebug \
+    genesis_violet-ap1a-user
