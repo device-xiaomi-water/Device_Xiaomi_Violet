224a46578cc0d67a0c315b741b589505dab7bf0c
ninja-ninja-arch <rajustha8027414@gmail.com>
2025-03-24 12:00:00 -0700

    Voilet :- welcome aospb

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,14 +1,14 @@
 #
-# Copyright (C) 2018-2020 The fortuneOS Project
+# Copyright (C) 2018-2020 The aospbOS Project
 # Copyright (C) 2020 The PixelExperience Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/fortune_violet.mk
+    $(LOCAL_DIR)/aospb_violet.mk
 
 COMMON_LUNCH_CHOICES := \
-    fortune_violet-ap1a-eng \
-    fortune_violet-ap1a-userdebug \
-    fortune_violet-ap1a-user
+    aospb_violet-ap2a-eng \
+    aospb_violet-ap2a-userdebug \
+    aospb_violet-ap2a-user
