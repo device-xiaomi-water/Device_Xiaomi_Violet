f6ebfbbb53897f714aea9459546750455a41e559
ninja-ninja-arch <rajustha8027414@gmail.com>
2025-03-24 12:01:06 -0700

    Voilet :- welcome aospb

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -118,7 +118,7 @@ USE_OPENGL_RENDERER := true
 # HIDL
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
     $(DEVICE_PATH)/framework_compatibility_matrix.xml \
-    vendor/fortune/config/device_framework_matrix.xml
+    vendor/aosp/config/device_framework_matrix.xml
 DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifest.xml
 DEVICE_MATRIX_FILE := hardware/qcom-caf/common/compatibility_matrix.xml
 DEVICE_FRAMEWORK_MANIFEST_FILE := $(DEVICE_PATH)/framework_manifest.xml
